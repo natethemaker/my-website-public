4a22eb69556436316f79c3d929d2649edd701719
natethemaker <79583517+natethemaker@users.noreply.github.com>
2021-04-23 09:26:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 var Name = prompt("Please put your name in here","name here")
-alert("Hi " + Name + " welcome to my website")
+
+if Name == "null"
+  
+  else
+    alert("hi " + Name + " welcome to my website")
