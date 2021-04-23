b3088dcf8df48ad4c11bf6ed924a2a83a59195a6
natethemaker <79583517+natethemaker@users.noreply.github.com>
2021-04-23 09:30:25 -0700

    fixed null is is "s

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var Name = prompt("Please put your name in here","name here")
 
-if (Name == "null") {
+if (Name == null) {
   alert("Welcome to my website")
 } else {
   alert("Hi " + Name + " welcome to my website")
